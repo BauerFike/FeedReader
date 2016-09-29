324e064a6f0c6e7cfa8e3059715d806fe03fbc99
BauerFike <luca.costanzi@gmail.com>
2016-09-29 08:37:37 +0200

    added limit to ArticleController

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -14,7 +14,7 @@ class ArticleController extends Controller
 {
     public function index()
     {
-			$articles = Article::with('Feed')->orderBy('pub_date','desc')->get();
+			$articles = Article::with('Feed')->orderBy('pub_date','desc')->limit(10)->get();
     	return view('feeds.articles',['articles'=>$articles]);
     }
 }
